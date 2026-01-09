2af7d67f9d67f7cbb64e85645efeb6ab7cb8c5dc
Jason Koch <jkoch@netflix.com>
2026-01-09 09:57:19 -0800

    perf: presize hashmap in Applications shuffleInstances

--- eureka-client/src/main/java/com/netflix/discovery/shared/Applications.java
+++ eureka-client/src/main/java/com/netflix/discovery/shared/Applications.java
@@ -20,7 +20,6 @@ import jakarta.annotation.Nullable;
 import java.util.AbstractQueue;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -32,7 +31,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
+import com.google.common.collect.Maps;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -333,8 +332,8 @@ public class Applications {
             @Nullable Map<String, Applications> remoteRegionsRegistry, 
             @Nullable EurekaClientConfig clientConfig,
             @Nullable InstanceRegionChecker instanceRegionChecker) {
-        Map<String, VipIndexSupport> secureVirtualHostNameAppMap = new HashMap<>();
-        Map<String, VipIndexSupport> virtualHostNameAppMap = new HashMap<>();
+        Map<String, VipIndexSupport> secureVirtualHostNameAppMap = Maps.newHashMapWithExpectedSize(this.secureVirtualHostNameAppMap.size());
+        Map<String, VipIndexSupport> virtualHostNameAppMap = Maps.newHashMapWithExpectedSize(this.virtualHostNameAppMap.size());
         for (Application application : appNameApplicationMap.values()) {
             if (indexByRemoteRegions) {
                 application.shuffleAndStoreInstances(remoteRegionsRegistry, clientConfig, instanceRegionChecker);
